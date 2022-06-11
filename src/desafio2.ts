294e0cb9e32e5302003c9267b0ce5e05d42a381d
Denis Souza <denis.souza@ulteriorti.com.br>
2022-06-11 19:09:52 -0300

    Exercicio 3 feito

--- src/desafio2.ts
+++ src/desafio2.ts
@@ -1,23 +1,23 @@
 // Como podemos melhorar o esse código usando TS? 
-enum Profissao{
+enum ProfissaoT{
     Atriz,
     Padeiro
 }
 class Trabalhador{
     nome:string
     idade:number
-    profissao:Profissao
-    constructor(nome:string,idade:number,profissao:Profissao){
+    profissao:ProfissaoT
+    constructor(nome:string,idade:number,profissao:ProfissaoT){
         this.nome = nome
         this.idade = idade
         this.profissao = profissao
     }
 }
 
-let pessoa1 =new Trabalhador("maria",29,Profissao.Atriz)
+let pessoa1 =new Trabalhador("maria",29,ProfissaoT.Atriz)
 
-let pessoa2 = new Trabalhador("Roberto",19,Profissao.Padeiro)
+let pessoa2 = new Trabalhador("Roberto",19,ProfissaoT.Padeiro)
 
-let pessoa3 = new Trabalhador("Laura",32,Profissao.Atriz)
+let pessoa3 = new Trabalhador("Laura",32,ProfissaoT.Atriz)
 
-let pessoa4 = new Trabalhador("Carlos",19,Profissao.Padeiro)
+let pessoa4 = new Trabalhador("Carlos",19,ProfissaoT.Padeiro)
